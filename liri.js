7d2693574f052af9d398b50608c72c6402cf4542
Carol Martin <carolmartin@cox.net>
2018-07-24 06:58:38 -0700

    added twitter code

--- liri.js
+++ liri.js
@@ -1,14 +1,18 @@
 // Read and set any environmnet variables with dotenv package
+
 require("dotenv").config();
 
-//Import keys.js and store in a variable 
-// import './keys';
-// import keys from './keys';
+var request = require("request");
 
-import * as keys from '/keys.js';
+var keys = require('./keys.js');
 
-var spotify = new Spotify(keys.spotify);
-var client = new twitter(keys.twitter);
+var Twitter = require("twitter");
+var client = new Twitter(keys.twitter);
+
+var Spotify = require("node-spotify-api");
+var spotifyVar = new Spotify(keys.spotify);
+
+
 
 //Take in commands and process accordingly
 
@@ -16,20 +20,48 @@ var operation = process.argv[2];
 
 
 console.log(operation);
-if (operation === "my-tweets"){
+if (operation === "my-tweets") {
     console.log("my-tweets");
-}
-else if (operatrion === "spotify-this-song"){
-    console.log("spotify-this-song");
+    myTweets();
 
 }
-else if (operatrion === "movie-this"){
+else if (operation === "spotify-this-song") {
+    console.log("spotify-this-song");
+    spotifySong();
+
+}
+else if (operation === "movie-this") {
     console.log("movie-this");
 
 }
-else if (operatrion === "do-what-it-says"){
+else if (operation === "do-what-it-says") {
     console.log("do-what-it-says");
 
 }
 
+function myTweets() {
+    numTweets = 0;
+    console.log("in MyTweets function");
+    var params = { screen_name: 'carolmartin9128' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            // console.log(tweets);
+            if (tweets.length > 20) {
+                numTweets = 20
 
+            }
+            else {
+                numTweets = tweets.length;
+            }
+            for (i = 0; i < numTweets; i++){
+                console.log(tweets[i].text);
+                console.log(tweets[i].created_at);
+            }
+
+        }
+    });
+  }
+
+function spotifySong() {
+    
+}
