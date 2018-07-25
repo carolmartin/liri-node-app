72f3f058052c0a2ad885bfcb0de16ff8a24ce190
Carol Martin <carolmartin@cox.net>
2018-07-25 08:53:31 -0700

    some updates to movie

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 // Read and set any environmnet variables with dotenv package
 
 require("dotenv").config();
+var fs = require("fs");
 
 var request = require("request");
 
@@ -12,11 +13,23 @@ var client = new Twitter(keys.twitter);
 var Spotify = require("node-spotify-api");
 var spotifyVar = new Spotify(keys.spotify);
 
+var OmdbApi = require('omdb-api-pt');
+ 
+// Create a new instance of the module.
+var omdb = new OmdbApi({
+    apiKey: 'trilogy',   // Your API key.
+    baseUrl: 'https://omdbapi.com'   // The base url of omdb. Defaults to 'https://omdbapi.com/'.
+  });
 
 
 //Take in commands and process accordingly
 
 var operation = process.argv[2];
+var parameter = process.argv[3];
+console.log("parameter: " + parameter);
+
+var songName = "";
+var movieName = "";
 
 
 console.log(operation);
@@ -27,11 +40,23 @@ if (operation === "my-tweets") {
 }
 else if (operation === "spotify-this-song") {
     console.log("spotify-this-song");
-    spotifySong();
+    // spotifySong();
+    songName = parameter;
+    if (process.argv.length < 4) {
+        songName = "The Sign";
+    }
+    getSpotifyInfo();
 
 }
 else if (operation === "movie-this") {
     console.log("movie-this");
+    movieName = parameter;
+    if (process.argv.length < 4) {
+        movieName = "Mr. Nobody";
+    }
+    getMovieInfo(movieName);
+    
+
 
 }
 else if (operation === "do-what-it-says") {
@@ -63,21 +88,44 @@ function myTweets() {
 }
 
 function spotifySong() {
-    spotifyVar.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+
+    var outputArray = [];
+    var songName;
+
+    fs.readFile("random.txt", 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        // random.txt contains a record with the command spotify-this-song
+        // followed by a comma and a song name
+        // Load the song name into output and than run spotify search
+        console.log("data " + data);
+        outputArray = data.split(",");
+
+        console.log("outputArray length: " + outputArray.length);
+        for (var i = 0; i < outputArray.length; i++) {
+            console.log(outputArray[i]);
+
+        }
+        var x = 1;
+        songName = outputArray[x];
+
+        console.log("song name: " + outputArray[x]);
+
+        getSpotifyInfo();
+
+
+
+        // spotifyVar.search({ type: 'track', query:'I Want it That Way'}, function (err, data) {
+
+    });
+
+}
+function getSpotifyInfo() {
+    spotifyVar.search({ type: 'track', query: songName }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
-        //   console.log(spotifyVar[0]);
-        // console.log("data length: " + (data.tracks.items.length));
-
-        // console.log(data.tracks);
-        // console.log("Song name: " + data.tracks.items[0].name);
-        // console.log("Artist: " + JSON.stringify(data.tracks.items[0].artists[0], null, 2));
-        // console.log("Artist-name: " + JSON.stringify(data.tracks.items[0].artists[0].name));
-        // console.log("Preview link of song: " + data.tracks.items[0].href);
-        // console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
-
 
         if (data.tracks.items.length > 20) {
             numSpotify = 20;
@@ -98,4 +146,26 @@ function spotifySong() {
 
         }
     });
+}
+function getMovieInfo(movieName) {
+    omdb.byId({
+        imdb: '',
+        title: movieName,
+        type: 'movie',
+        year: '',
+        plot: 'full',
+        tomatoes: true
+      }).then(res => console.log(res))
+        .catch(err => console.error(err))
+
+        console.log(JSON.stringify(res.Title) + " " + JSON.stringify(omdb[0].Year) + " Rated" + JSON.stringify(omdb[0].Rated)  + " tomato Rating: " + JSON.stringify(omdb[0].tomatoRating));
+
+    // omdb.bySearch({
+    //     search: movieName,
+    //     type: 'movie',
+    //     page: 1
+    //   }).then(res => console.log(res))
+    //     .catch(err => console.error(err))
+
+    
 }
